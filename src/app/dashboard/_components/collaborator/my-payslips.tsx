a0fd7b252c8480671553e275f80e5a891653c3ca
sidney serrao <sidthay06072018@gmail.com>
2025-09-26 18:15:04 +0000

    Try fixing this error: `Runtime Error: Error: Event handlers cannot be p

--- src/app/dashboard/_components/collaborator/my-payslips.tsx
+++ src/app/dashboard/_components/collaborator/my-payslips.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { GlassCard, CardHeader, CardTitle, CardContent } from '@/components/glass-card';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Button } from '@/components/ui/button';
