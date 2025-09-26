99f16447f70349948b34bc8e5b3c405b195843d0
sidney serrao <sidthay06072018@gmail.com>
2025-09-26 06:03:46 +0000

    quero que voce substitua esse nome pela logomarca da empresa, a logo est

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -2,8 +2,7 @@ import type { SVGProps } from 'react';
 import Image from 'next/image';
 
 export function Logo(props: React.ImgHTMLAttributes<HTMLImageElement>) {
-  // TODO: Adicione o seu arquivo de logo (ex: logo.png) à pasta /public na raiz do projeto.
-  // O caminho abaixo já está configurado para funcionar assim que o arquivo for adicionado.
+  // O logo.png deve estar na pasta /public na raiz do projeto.
   const logoUrl = "/logo.png";
 
   return (
