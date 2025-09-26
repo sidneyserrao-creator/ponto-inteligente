c47aa9de3339746ca059002cda81d7977b7ef3d5
sidney serrao <sidthay06072018@gmail.com>
2025-09-26 05:58:52 +0000

    esta aqui a URL da imagem da logo:  https://drive.google.com/file/d/18-G

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -2,9 +2,9 @@ import type { SVGProps } from 'react';
 import Image from 'next/image';
 
 export function Logo(props: React.ImgHTMLAttributes<HTMLImageElement>) {
-  // TODO: Substitua a URL abaixo pela URL do seu arquivo de logo PNG.
-  // Se você adicionar seu logo à pasta /public, o caminho será "/seu-logo.png".
-  const logoUrl = "https://placehold.co/160x28/0f172a/FFF?text=BIT+Seguran%C3%A7a";
+  // TODO: Adicione o seu arquivo de logo (ex: logo.png) à pasta /public na raiz do projeto.
+  // O caminho abaixo já está configurado para funcionar assim que o arquivo for adicionado.
+  const logoUrl = "/logo.png";
 
   return (
     <img
