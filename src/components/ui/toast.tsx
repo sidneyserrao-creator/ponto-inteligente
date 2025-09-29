5d2ac7b7cfb8dace133d044893f81c80f8b3dc72
sidney serrao <sidthay06072018@gmail.com>
2025-09-29 02:12:20 +0000

    mude para um efeito vidro fosco (_for element <Primitive.li>_)

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -29,9 +29,10 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default:
+          "border bg-card/60 backdrop-blur-md text-foreground",
         destructive:
-          "destructive group border-destructive bg-destructive text-destructive-foreground",
+          "destructive group border-destructive bg-destructive/60 backdrop-blur-md text-destructive-foreground",
       },
     },
     defaultVariants: {
