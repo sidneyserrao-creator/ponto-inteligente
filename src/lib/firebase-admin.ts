b43ca5ebae441f01145149ea602c8572e0ea1e07
sidney serrao <sidthay06072018@gmail.com>
2025-10-01 13:27:20 +0000

    Try fixing this error: `Runtime Error: Error: Service account object mus

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,4 +1,3 @@
-import 'dotenv/config';
 import admin from 'firebase-admin';
 
 // Verifica se o Firebase já foi inicializado para evitar erros.
@@ -7,7 +6,7 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
     }),
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   });
